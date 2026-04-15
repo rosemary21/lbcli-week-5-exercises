732db221c5c19afa31aff1e50241ee2790444fdf
MAC <mac@MACs-MacBook-Pro.local>
2026-04-15 22:12:37 +0100

    exercise5

--- submission/04.sh
+++ submission/04.sh
@@ -1,5 +1,3 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-bitcoin-cli deriveaddresses "pkh(02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277)#qsd9m79m"
-
-bitcoin-cli getaddressinfo <address_from_step1>
+echo "0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
